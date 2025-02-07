9a4358a02237807d7186a9aac2c91237f606c849
Hongwei <gmhwxi@gmail.com>
2025-02-06 22:29:39 -0500

    Updating: very very minorly

---
+++ lectures/lecture-02-06/ArrayStack.java
@@ -0,0 +1,34 @@
+public class ArrayStack<T> {
+
+    static public class Overflow
+	extends Exception {
+    }
+    static public class Underflow
+	extends Exception {
+    }
+    
+    private static final int INIT_CAPACITY = 8;
+    private T[] a; // array of items
+    private int n; // number of elements on stack
+
+    public int size() {
+        return n;
+    }
+    public boolean isEmpty() {
+        return n == 0;
+    }
+    public boolean isFull() {
+        return n >= a.length;
+    }
+
+    public
+    void push(T item) throws Overflow {
+        if (isFull()) throw new Overflow();
+	a[n] = item; n += 1; return;
+    }
+    public T pop() throws Underflow {
+        if (isEmpty()) throw new Underflow();
+	n -= 1;
+        T item = a[n]; a[n] = null; return item;
+    }
+}
