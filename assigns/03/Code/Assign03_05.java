825e0fdf480ae74408a139d1af7dda7f9437ec5a
Hongwei <gmhwxi@gmail.com>
2025-02-26 01:12:19 -0500

    Updating: very very minorly

---
+++ assigns/03/Code/Assign03_05.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public
+class
+Assign03_05
+<T extends Comparable<T>>
+implements ArraySorter<T> {
+
+    public void sort(T[] A) {
+	// please give a RECURSIVE implementation of INSERTION sort
+    }
+    public ArraySorter<T> ArraySorter() {
+	// [ArraySorter] wrap the method [sort] inside an object
+    }
+}
---
+++ assigns/03/README.md
@@ -0,0 +1,13 @@
+# Assignment 2
+
+It is due Monday, the 3rd of March
+
+## Assignment 3-1 (10 points)
+
+## Assignment 3-2 (30 points)
+
+## Assignment 3-3 (20 points)
+
+## Assignment 3-4 (20 points)
+
+## Assignment 3-5 (30 points)
