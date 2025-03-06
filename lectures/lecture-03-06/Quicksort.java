db7598ea65861e62947d7f612c32bcd142121c5c
Hongwei <gmhwxi@gmail.com>
2025-03-06 16:51:12 -0500

    Updating: very very minorly

---
+++ lectures/lecture-03-06/Quicksort.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+public class Quicksort {
+
+    // This class should not be instantiated.
+    private Quicksort() { }
+
+    private static <T> void exch(T[] A, int i, int j) {
+	T tmp;
+	tmp = A[i]; A[i] = A[j]; A[j] = tmp; return;
+    }
+
+    private static <T extends Comparable<T>> boolean less(T x, T y) {
+	return (x.compareTo(y) < 0);
+    }
+
+}
